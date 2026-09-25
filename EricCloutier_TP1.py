fc1e36a389ab1808966dffde19f87e5b69dd944d
EricClout <ecloutier1@etu.uqac.ca>
2026-09-25 10:53:31 -0400

    Ajout de code + modif gitignore
    gitignore a été modifié pour ignorer les fichier json et venv
    
    fichier TP1, ajout du code pour sécuriser l'ajout du fichier json à loader

--- EricCloutier_TP1.py
+++ EricCloutier_TP1.py
@@ -1,3 +1,22 @@
-name = input("Enter your name: ")
+import sys
+import json
 
-print("allo " + name + "!")
+# Vérifie s'il y a fichier JSON d'entré pour l'ouvrir
+if len(sys.argv) < 2:
+    print("Attention, Aucun fichier JSON de fourni")
+    sys.exit(1)
+
+json_path = sys.argv[1]
+print(f"<<<< Fichier JSON bien reçu: ''{json_path}'' >>>>")
+
+# Charge le fichier JSON sécuritairement (try / except)avec affichage d'erreur si le fichier n'est pas trouvé
+try:
+    file = open(json_path, "r", encoding="utf-8")
+    donnees = json.load(file)
+    file.close()
+
+    print(f"<<<< Chargement complété ! Nombre d'éléments : {len(donnees)} >>>>")
+
+except Exception as e:
+    print(f"<<<< Erreur, le fichier n'a pas chargé correctement {json_path} : {e} >>>>")
+    sys.exit(1)
